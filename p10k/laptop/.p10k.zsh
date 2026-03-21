f0498c09ff1e5d006681765ee1500e4105cdde9d
Bassem Karoui <23449512+bassemkaroui@users.noreply.github.com>
2026-03-21 09:37:22 +0100

    add delta in git config and fix small bug in oh-my-tmux

--- p10k/laptop/.p10k.zsh
+++ p10k/laptop/.p10k.zsh
@@ -186,9 +186,8 @@
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=''
   # Custom icon.
-  # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
-  # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=''
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
